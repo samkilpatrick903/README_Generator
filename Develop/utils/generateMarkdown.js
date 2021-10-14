92990e6f7c60c8bb63126b0827da6f3f45a82c27
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-10-13 22:00:44 -0500

    Not working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return (license === 'None' ? ""
+            : `![License: ${license}](https://img.shields.io/badge/License-${license.split(' ').join('%20')}-blue.svg)`)
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -24,13 +27,28 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return (license === 'None' ? "" : `This repo is licensed under the ${license} license.`);
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  const licenseBadge = assignLicenseLink(data.license);
+
+  return `# ${data.title}\n` +
+        `## Description\n` +
+        `${data.description}\n` +
+        `## Installation\n` +
+        `${data.installation}\n` +
+        `## Usage\n` +
+        `${data.usageInformation}\n` +
+        `## Contributing\n` +
+        `${data.contributionGuidelines}\n` +
+        `## Tests\n` +
+        `${data.testInstructions}\n` +
+        `## License\n` +
+        `${licenseBadge}\n`
 }
 
 module.exports = generateMarkdown;
